dbdab55ff9603bf94a95f0cb416ae9c847e5441e
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-13 09:41:54 -0700

    Update Dice Rolling.py

--- Dice Rolling.py
+++ Dice Rolling.py
@@ -1,2 +1,8 @@
 import random
 
+rolls = int(input("How mny times do you want to roll? The dice is out of 20: "))
+
+while rolls > 0:
+    number = random.randint(1, 20)
+    print(f"Your roll was {number} {rolls}")
+    rolls -= 1

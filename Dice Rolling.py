a02a5361fefff5923ff7320f2a63a20465341ba3
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-13 10:13:05 -0700

    Update Dice Rolling.py

--- Dice Rolling.py
+++ Dice Rolling.py
@@ -1,12 +1,17 @@
 import random
 
-rolls = int(input("How many times do you want to roll? "))
-dice = int(input("Enter the number that you want the dice to be out of: "))
+try:
+    rolls = int(input("How many times do you want to roll? "))
+    dice = int(input("Enter the number that you want the dice to be out of: "))
 
-if dice > 0:
-    while rolls > 0:
-        number = random.randint(1, dice)
-        print(f"Your roll was {number}")
-        rolls -= 1
-else:
-    print("Please enter a valid number greater than 0 for the dice sides.")
+    if rolls <= 0:
+        print("Please enter a valid number greater than 0 for the number of rolls.")
+    elif dice <= 0:
+        print("Please enter a valid number greater than 0 for the dice sides.")
+    else:
+        while rolls > 0:
+            number = random.randint(1, dice)
+            print(f"Your roll was {number}")
+            rolls -= 1
+except ValueError:
+    print("Please use numbers")

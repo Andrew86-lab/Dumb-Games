0082d4798297e1eda557f8a27b73f693eb36c840
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-24 10:21:56 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,7 @@ import math
 def get_user_input():
     while True:
         try:
-            user_input = int(input("What do you want? 1 for Multiply, 2 for Divide, Add, 3 for Subtract, 4 for Square, 5 for Square Root, or 6 for Exponentiate? "))
+            user_input = int(input("What do you want? 1 for Multiply, 2 for Divide, 3 for Add, 4 for Subtract, 5 for Square, 6 for Square Root, or 7 for Exponentiate? "))
             if user_input in range(1, 8):
                 return user_input
             else:
@@ -25,3 +25,18 @@ def mutliply(user_input):
                 print("Please enter a number instead of a whole number.")
 
 mutliply(user_input)
+
+def divide(user_input):
+    if user_input == 2:
+        while True:
+            try:
+                num1 = list(map(float, input("Enter the numbers you want to divide: ").split()))
+                first_number = num1.pop(0)
+                for x in num1:
+                    first_number /=  x
+                print(f"The answer is {first_number}")
+                break
+            except ValueError:
+                print("Please enter a number instead of a whole number.")
+
+divide(user_input)

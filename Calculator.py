7874fb5f164c6e3aee0deb5d401586b22c338663
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-24 10:33:10 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -3,13 +3,13 @@ import math
 def get_user_input():
     while True:
         try:
-            user_input = int(input("What do you want? 1 for Multiply, 2 for Divide, 3 for Add, 4 for Subtract, 5 for Square, 6 for Square Root, or 7 for Exponentiate? "))
-            if user_input in range(1, 8):
+            user_input = int(input("What do you want? 1 for Multiply, 2 for Divide, 3 for Add and Subtract, 4 for Square, 5 for Square Root, or 6 for Exponentiate? "))
+            if user_input in range(1, 7):
                 return user_input
             else:
-                print("Please enter a number between 1 and 7")
+                print("Please enter a number between 1 and 6")
         except ValueError:
-            print("Please enter a number instead of a letter or word and whole numbers.")
+            print("Please enter a number instead of a letter or word, please make it a whole number.")
         
 user_input = get_user_input()
 
@@ -22,7 +22,7 @@ def mutliply(user_input):
                 print(f"The answer is {result}")
                 break
             except ValueError:
-                print("Please enter a number instead of a whole number.")
+                print("Please enter a number instead of a letter or a word.")
 
 mutliply(user_input)
 
@@ -37,6 +37,19 @@ def divide(user_input):
                 print(f"The answer is {first_number}")
                 break
             except ValueError:
-                print("Please enter a number instead of a whole number.")
+                print("Please enter a number instead of a letter or a word.")
 
 divide(user_input)
+
+def add_subtract(user_input):
+    if user_input == 3:
+        while True:
+            try:
+                num1 = list(map(float, input("Enter the numbers you want to add or subract: ").split()))
+                result = sum(num1)
+                print(f"The answer is {result}")
+                break
+            except ValueError:
+                print("Please enter a number instead of a letter or a word.")
+
+add_subtract(user_input)

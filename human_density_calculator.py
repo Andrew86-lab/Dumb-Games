6c966c809b6286241bdc5d9ed6dbdf62a168cf7c
Andrew86-lab <andrewlgilljr@gmail.com>
2025-01-30 09:11:43 -0700

    Update human_density_calculator.py

--- human_density_calculator.py
+++ human_density_calculator.py
@@ -5,7 +5,7 @@ import math
 lbs_weight = float(input("Enter your character's weight in pounds (NUMBERS ONLY): "))
 
 volume = lbs_weight * 0.00045
-mass = lbs_weight * 0.453592
+mass = lbs_weight * 0.453592  # This turns it into pounds from Kilograms, you already made sure the enterd there weight in pounds.
 density = math.ceil(mass / volume)
 print("V:", volume, "m^3")
 print("M:", mass, "kg")

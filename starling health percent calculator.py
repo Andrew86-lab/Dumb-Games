f25892bd44216bf1a7958af1497ea555a0c31a63
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-30 10:01:20 -0700

    changed some variable names, added current hp separate from max

--- starling health percent calculator.py
+++ starling health percent calculator.py
@@ -5,25 +5,30 @@ Enter numbers only unless instructed otherwise.
 """)
 time.sleep(0.75)
 
-base_max = int(input("Input your character's BASE max health: "))
-base_weight = float(input("Input your character's BASE weight in pounds: "))
+base_max_hp = int(input("Input your character's BASE max health: ").strip())
+base_weight = float(input("Input your character's BASE weight in pounds: ").strip())
+base_current_hp = int(input("Input your character's current health points: ").strip())
 
-current_max = base_max
+current_max_hp = base_max_hp
 current_weight = base_weight
+current_hp = base_current_hp
 
 def percent_finder(weight_change):
-    global current_max
+    global current_max_hp
     global current_weight
+    global current_hp
     weight_percent = round(((weight_change + current_weight) / current_weight), 2)
     print(weight_percent)
-    current_max = current_max * weight_percent
+    current_max_hp = current_max_hp * weight_percent
     current_weight += weight_change
-    return current_max
+    current_hp = current_hp * weight_percent
+    return current_max_hp, current_hp, current_weight
 ender = input("How much are you adding to the weight in pounds (negative when subtracting, 'end' when done): ").lower().strip()
-print(f"Your base max health is {round(base_max)}.")
+print(f"Your base max health is {round(base_max_hp)}.")
 print(f"Your base weight is {base_weight} pounds.")
 while ender != "end":
     percent_finder(float(ender))
-    print(f"Your current max health is now {round(current_max)}.")
+    print(f"Your current max health is now {round(current_max_hp)}.")
+    print(f"Your current healh is now {round(current_hp)}.")
     print(f"Your current weight is now {current_weight} pounds.")
     ender = input("How much are you adding to the weight in pounds (negative when subtracting, 'end' when done): ").lower().strip()

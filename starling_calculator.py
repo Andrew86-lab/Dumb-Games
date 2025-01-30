c25ae5fc923fa84e4999fc9a0ce382011dfee207
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-30 08:50:30 -0700

    Right now it's just a volume mass and density calculator. To be used for making a homebrew calculator project

---
+++ starling_calculator.py
@@ -0,0 +1,19 @@
+#Created by Kai Reinhold (kaireinhold on GitHub)
+
+import math
+
+lbs_weight = float(input("Enter your character's weight in pounds (NUMBERS ONLY): "))
+
+volume = lbs_weight * 0.00045
+mass = lbs_weight * 0.453592
+density = math.ceil(mass / volume)
+print("V:", volume, "m^3")
+print("M:", mass, "kg")
+print("D:", density, "kg/m^3")
+
+
+
+
+
+
+#This was actually a lot easier than I thought ?

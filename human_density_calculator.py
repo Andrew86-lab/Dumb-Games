d90b6c60f62991a94457076467de4ed82c9e73d2
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-30 09:43:23 -0700

    fixed calculations. added 'round' function to prevent infinite decimals, but doesn't round to whole numbers.

--- human_density_calculator.py
+++ human_density_calculator.py
@@ -4,9 +4,9 @@ import math
 
 lbs_weight = float(input("Enter your character's weight in pounds (NUMBERS ONLY): "))
 
-volume = lbs_weight * 0.00045
-mass = lbs_weight * 0.453592  # This turns it into pounds from Kilograms, you already made sure the enterd there weight in pounds.
-density = math.ceil(mass / volume)
+volume = round(lbs_weight * 0.00045, 4)
+mass = round(lbs_weight / 2.205, 2)
+density = round(mass / volume, 4)
 print("V:", volume, "m^3")
 print("M:", mass, "kg")
 print("D:", density, "kg/m^3")

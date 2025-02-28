166ee20860614fb8a4511e8272485ca8042f4cf0
Andrew86-lab <andrewlgilljr@gmail.com>
2025-02-28 10:08:53 -0700

    This is going to be a pain

---
+++ Uno.py
@@ -0,0 +1,11 @@
+# Created by Andrewlab-86, 2/28/25
+
+print("We are going to play a game of UNO ready.")
+
+# there are 108 cards in a deck of UNO cards
+# red: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, skip, reverse, draw two
+# yellow: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, skip, reverse, draw two
+# green: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, skip, reverse, draw two
+# blue: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, skip, reverse, draw two
+# there are 4 of each for wild cards and draw 4 wild cards
+

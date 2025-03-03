5d2a12aefd3af5cfd4a469c52553bf054e262de6
Andrew86-lab <andrewlgilljr@gmail.com>
2025-03-03 08:27:16 -0700

    Update Uno.py

--- Uno.py
+++ Uno.py
@@ -8,6 +8,7 @@ print("We are going to play a game of UNO ready.")
 # green: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, 2 of each for skip, reverse, draw two
 # blue: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, ,2 , 3, 4, 5, 6, 7, 8, 9, 2 of each for skip, reverse, draw two
 # there are 4 of each for wild cards and draw 4 wild cards
+# rules: first person to get rid of there cards first wins
 
 uno_cards = {
     "red": {0: 1, **dict.fromkeys(range(1, 10), 2), "skip": 2, "reverse": 2, "draw_two": 2},

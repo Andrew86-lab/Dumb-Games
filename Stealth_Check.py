593f51ab543d1a0277db96be5439f0d98933779d
Andrew86-lab <andrewlgilljr@gmail.com>
2025-03-07 08:39:21 -0700

    Update Stealth_Check.py

--- Stealth_Check.py
+++ Stealth_Check.py
@@ -14,23 +14,23 @@ except ValueError:
 
 proficiency_bonus = math.ceil((user_level / 4) + 1)
 
-user_input = input("Do you know your character's dexterity modifier? (Y/N) ").lower()
+user_input = input("Do you know your character's dexterity modifier? (Y/N) ").lower().strip()
 
-stealth_proficiency = input("Do you have proficiency in the Stealth skill? (Y/N) ").lower()
+stealth_proficiency = input("Do you have proficiency in the Stealth skill? (Y/N) ").lower().strip()
 stealth_expertise = "n"
 if stealth_proficiency == "y":
-    stealth_expertise = input("Do you have expertise in the Stealth skill? (Y/N) ").lower()
+    stealth_expertise = input("Do you have expertise in the Stealth skill? (Y/N) ").lower().strip()
 
 user_dexterity = 0
 if user_input == "y":
     try:
-        user_dexterity = int(input("What is your character's dexterity modifier? "))
+        user_dexterity = int(input("What is your character's dexterity modifier? ")).strip()
     except ValueError:
         print("Invalid input. Please enter a number for the dexterity modifier.")
         exit()
 elif user_input == "n":
     try:
-        dex_score = int(input("What is your character's dexterity score? "))
+        dex_score = int(input("What is your character's dexterity score? ")).strip()
         user_dexterity = (dex_score - 10) // 2
     except ValueError:
         print("Invalid input. Please enter a number for the dexterity score.")
